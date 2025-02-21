f27bba01181e905cd15009994ff85dcc80957766
Elankathir <esekar@diligentsquad.com>
2025-02-21 12:30:24 +0530

    created components for view page in shared

---
+++ src/app/components/shared/view-template/view-sub-content/view-sub-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-sub-content',
+  imports: [],
+  templateUrl: './view-sub-content.component.html',
+  styleUrl: './view-sub-content.component.css'
+})
+export class ViewSubContentComponent {
+
+}
